3c092fba797f0cdd8cd8e2891b60055da9f2c388
tanisha0605 <tanishakanal@gmail.com>
2024-05-05 12:43:53 +0530

    First Commit

---
+++ practice1.py
@@ -0,0 +1,6 @@
+from tkinter import *
+window=Tk()
+window.geometry("420x420")
+window.title("Practice 1")
+window.config(background="#5cfcff")
+window.mainloop()
---
+++ practice2.py
@@ -0,0 +1,21 @@
+from tkinter import *
+window = Tk()
+
+#photo = PhotoImage(file='images.png')
+
+label = Label(window,
+              text="Hello World",
+              font=('Arial',40,'bold'),
+              fg='#00FF00',
+              bg='black',
+              relief=RAISED,
+              bd=10,
+              padx=20,
+              pady=20,
+              #image=photo,
+              #compound='bottom'
+              )
+label.pack()
+#label.place(x=0,y=0)
+
+window.mainloop()
